f1c4f31416bdb7a87b2c7d066794e3cca61dc093
a1406yy@aiit.ac.jp <yueyuan@YUEYUAN-no-MacBook-Pro.local>
2015-09-24 22:41:13 +0900

    add WebContent

---
+++ WebContent/js/code8_2.js
@@ -0,0 +1,66 @@
+
+var map;
+var directions;
+
+function initialize() {
+  if (GBrowserIsCompatible()) {
+    map = new GMap2(document.getElementById("map_canvas"));
+    map.setCenter(new GLatLng(35.670756, 139.823215), 13);
+
+    directions = new GDirections(map, document.getElementById("route"));
+
+    GEvent.addListener(directions, "load", onGDirectionsLoad);
+    GEvent.addListener(directions, "addoverlay", onGDirectionsAddOverlay);
+    GEvent.addListener(directions, "error", onGDirectionsError);
+  }
+}
+
+function dispRoute() {
+  var from = document.getElementById("from").value;
+  var to = document.getElementById("to").value;
+
+  var flag;
+
+  
+  var option;
+  if (document.getElementById("flagTravelMode").value == "0"){
+	  option = {locale: 'ja_JP', travelMode: G_TRAVEL_MODE_WALKING};
+  }else if (document.getElementById("flagTravelMode").value == "1"){
+	  //option = {locale: 'ja_JP', travelMode: G_TRAVEL_MODE_DRIVING};
+	  
+	  if (document.getElementById("highway").value == "0"){
+		    flag = false;
+		    
+		  }else{
+		    flag = true;
+		  }
+	  option = {locale: 'ja_JP', avoidHighways: flag,travelMode: G_TRAVEL_MODE_DRIVING};
+  }
+  
+  directions.clear();
+
+  var pointArray = [from,to];
+  //var option = {locale: "ja_JP", avoidHighways: flag, G_TRAVEL_MODE_DRIVING: flagTravelM};
+  //var option = {locale: 'ja_JP', travelMode: G_TRAVEL_MODE_DRIVING};
+  //var option = {locale: 'ja_JP', avoidHighways: flag, travelMode: G_TRAVEL_MODE_DRIVING};
+  
+
+  directions.loadFromWaypoints(pointArray, option);
+  
+  
+}
+
+function onGDirectionsLoad(){ 
+  alert("ルート案内の表示を開始します");
+}
+
+function onGDirectionsAddOverlay(){ 
+  alert("ルート案内の表示が終了しました");
+}
+
+function onGDirectionsError(){ 
+  var errCode = directions.getStatus().code;
+  document.getElementById('route').innerHTML = "エラーが発生しました:" + errCode;
+}
+
+
